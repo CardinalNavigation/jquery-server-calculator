ac9b92e2879a841b1ba756e553861a753c35c811
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-25 21:18:12 -0500

    getting stuck, cant figure out how to get operator button presses into my package with the inputs

--- server/public/client.js
+++ server/public/client.js
@@ -3,4 +3,35 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log("Client side javascript works!");
+    $('#addition').on('click', packageInput)
+    $('#addition').on('click', additiontoTrue)
+    // $('#subtraction').on('click', packageInput)
+    // $('#multiplication').on('click', packageInput)
+    // $('#division').on('click', packageInput)
+    $('#equalSign').on('click', packageInput)
+    // $('#clear').on('click', packageInput)
+
 }
+
+
+function packageInput(event) {
+    event.preventDefault();
+    console.log("button working")
+    let input1 = $("#input1").val()
+    let input2 = $("#input1").val()
+
+    let addition = false;
+   
+    let combinedInputObject = {
+        input1: Number(input1),
+        input2: Number(input2),
+        addition: false,
+        subtraction: false,
+        multiplication: false,
+        division: false,
+    }
+    console.log(combinedInputObject)
+    // What do i want to happen when the addition button is pressed?
+}
+
+
