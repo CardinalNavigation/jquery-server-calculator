43eab97813a3b61f5149361daec4895bdfca8bc2
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-25 20:09:12 -0500

    Files Set, HTML next

---
+++ server/public/client.js
@@ -0,0 +1,6 @@
+$(document).ready(onReady);
+
+
+function onReady() {
+    console.log("Client side javascript works!");
+}
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const data = require('./modules/data.js');
+
+const express = require('express');
+let bodyParser = require('body-parser');
+const app = express();
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+// Routes to here
+app.listen(5000, function () {
+    console.log(`You started the server! It is running on port 5000.`);
+})
