0a92b6002062db56591701b188e4174eeb7fb8d8
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-25 20:33:50 -0500

    most of HTML set up now

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-
 // Routes to here
 app.listen(5000, function () {
     console.log(`You started the server! It is running on port 5000.`);
